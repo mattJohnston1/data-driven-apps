96c468ec11052bffaa3e508bff3dba2ad76cfbe2
Matt Johnston <emollientspy1@gmail.com>
2021-01-22 09:55:32 -0800

    part 1 finished

---
+++ routes.js
@@ -0,0 +1,12 @@
+// ./routes.js
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+   throw new Error('This is a test error!');
+  res.render('index', { title: 'Home' });
+});
+
+module.exports = router;
